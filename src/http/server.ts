57df46b6e5bdd93c0ce36aba8cb5a5185fc28fa5
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-09-11 21:19:47 -0300

    feat: adds goal creation

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
 
 const app = fastify()
+
+const createGoalBodySchema = z.object({
+  title: z.string(),
+  desiredWeeklyFrequency: z.number(),
+})
+
+app.post('/goals', async request => {
+  const body = createGoalBodySchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app
   .listen({
